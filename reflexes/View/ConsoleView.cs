2b0606923fad1b09b0650a37ad898b8748b40d64
gustavlandin <gl222gw@student.lnu.se>
2019-11-19 15:49:57 +0100

    Red

---
+++ reflexes/View/ConsoleView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace reflexes.View
+{
+    public class ConsoleView
+    {
+        public string _greeting = "Welcome to Reflexes!";
+
+        public void DisplayGreetingMessage()
+        {
+            //Console.WriteLine(_greeting);
+        }
+
+    }
+}
