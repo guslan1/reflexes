d262cbe6900f04a28e10e8e386f5607713a9f0f0
gustavlandin <gl222gw@student.lnu.se>
2019-11-22 10:27:25 +0100

    RED

---
+++ reflexes/Model/Alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reflexes.Model
+{
+    public class Alphabet
+    {
+    
+        public int WordsLeft()
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ reflexesTest/AlphabetTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using reflexes.Model;
+
+namespace reflexesTest
+{
+    public class AlphabetTest
+    {
+        [Fact]
+        public void WordsLeft_ShouldReturnNumberOfWordsInAlphabet()
+        {
+            Alphabet sut = new Alphabet();
+            int expected = 25;
+            int actual = sut.WordsLeft();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
