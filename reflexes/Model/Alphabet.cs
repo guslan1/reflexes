df26817b485a7721044337e9f2d0d197c314a7ed
gustavlandin <gl222gw@student.lnu.se>
2019-11-22 10:28:41 +0100

    GREEN

--- reflexes/Model/Alphabet.cs
+++ reflexes/Model/Alphabet.cs
@@ -4,13 +4,46 @@ using System.Text;
 
 namespace reflexes.Model
 {
-    public class Alphabet
+    class Alphabet
     {
-    
+        private string[] _alphabetArray = new string[25] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z" };
+        private List<string> _randomAlphabet;
+
+        public Alphabet()
+        {
+            _randomAlphabet = RandomAlphabet(AlphabetList());
+        }
+
+        private List<string> RandomAlphabet(List<string> alphabetList)
+        {
+            List<string> randomAlphabetList = new List<string>();
+            Random r = new Random();
+            int randomIndex = 0;
+
+            while (alphabetList.Count > 0)
+            {
+                randomIndex = r.Next(0, alphabetList.Count);
+                randomAlphabetList.Add(alphabetList[randomIndex]);
+                alphabetList.RemoveAt(randomIndex);
+            }
+            return randomAlphabetList;
+        }
+
+        private List<string> AlphabetList()
+        {
+            List<string> alphabetList = new List<string>(25);
+            foreach (string character in _alphabetArray)
+            {
+                alphabetList.Add(character);
+            }
+            return alphabetList;
+        }
+
         public int WordsLeft()
         {
-            return 0;
+            return _randomAlphabet.Count;
         }
+
 
     }
 }
