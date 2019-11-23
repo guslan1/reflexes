7d39b7c6cf0668fa9083c27defd50238bf12796e
gustavlandin <gl222gw@student.lnu.se>
2019-11-23 11:18:21 +0100

    GREEN

--- reflexes/Model/ReflexGameImplemented.cs
+++ reflexes/Model/ReflexGameImplemented.cs
@@ -6,6 +6,11 @@ namespace reflexes.Model
 {
     public class ReflexGameImplemented : ReflexGame
     {
+        private Alphabet _alphabet;
+
+        public void StartGame(Alphabet alphabet) => _alphabet = alphabet;
+
+        public int WordsLeft() => _alphabet.WordsLeft();
 
 
     }
