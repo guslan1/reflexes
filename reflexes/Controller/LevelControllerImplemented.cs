dc656f36dc4229807669184ab845be7f9a03e676
gustavlandin <gl222gw@student.lnu.se>
2019-11-27 10:47:45 +0100

    GREEN

--- reflexes/Controller/LevelControllerImplemented.cs
+++ reflexes/Controller/LevelControllerImplemented.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using reflexes.Model;
+using reflexes.View;
+
+namespace reflexes.Controller
+{
+    public class LevelControllerImplemented : LevelController
+    {
+
+        private ReflexGame _reflexGame;
+        private ConsoleView _consoleView;
+
+        public LevelControllerImplemented(ReflexGame reflexGame, ConsoleView consoleView)
+        {
+            _reflexGame = reflexGame;
+            _consoleView = consoleView;
+        }
+        public bool EasyMode()
+        {
+            if (_reflexGame.IsGameCompleted())
+            {
+                _consoleView.GameCompleted();
+                _consoleView.DisplayPressAKeyToContinue();
+                _consoleView.ReadKey();
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
