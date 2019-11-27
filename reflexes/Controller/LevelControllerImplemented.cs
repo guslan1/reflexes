16e2bf4a72e055ddfc4c19beb14b532fb1b66cc4
gustavlandin <gl222gw@student.lnu.se>
2019-11-27 12:44:08 +0100

    GREEN

--- reflexes/Controller/LevelControllerImplemented.cs
+++ reflexes/Controller/LevelControllerImplemented.cs
@@ -28,6 +28,7 @@ namespace reflexes.Controller
             {
                 _consoleView.PresentLetter(_reflexGame.GetNewLetter());
                 _reflexGame.CreateStopwatch();
+                _reflexGame.StartStopwatch();
             }
             return true;
         }
