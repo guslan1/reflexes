090daaf9de8450de152817da6d21612ba9548f6f
gustavlandin <gl222gw@student.lnu.se>
2019-11-28 09:34:01 +0100

    GREEN

--- reflexes/Controller/MainController.cs
+++ reflexes/Controller/MainController.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using reflexes.Model;
+using reflexes.View;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reflexes.Controller
+{
+    public class MainController
+    {
+
+        private ReflexGame _reflexGame;
+        private ConsoleView _consoleView;
+        private LevelController _levelController;
+
+        public MainController(ReflexGame reflexGame, ConsoleView consoleView, LevelController levelController)
+        {
+            _reflexGame = reflexGame;
+            _consoleView = consoleView;
+            _levelController = levelController;
+        }
+
+    }
+}
