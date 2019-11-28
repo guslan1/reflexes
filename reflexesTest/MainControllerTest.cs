83a4d34f232ed049e00eed02bfc1af1d71398870
gustavlandin <gl222gw@student.lnu.se>
2019-11-28 09:32:27 +0100

    RED

---
+++ reflexesTest/MainControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+using Moq;
+using reflexes.Controller;
+using reflexes.Model;
+using reflexes.View;
+
+namespace reflexesTest
+{
+    public class MainControllerTest
+    {
+
+        [Fact]
+        public void MainController_NewMainControllerShouldReturnMainController()
+        {
+            var reflexGame = new ReflexGameImplemented();
+            var consoleView = new ConsoleViewImplemented();
+            var levelController = new LevelControllerImplemented(reflexGame, consoleView);
+            var mainController = new MainController(reflexGame, consoleView, levelController);
+
+            Assert.IsType<MainController>(mainController);
+        }
+
+    }
+}
