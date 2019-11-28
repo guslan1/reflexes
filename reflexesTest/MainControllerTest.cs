f2732e64f181929405e7fe70b1bf8390fdfb8c07
gustavlandin <gl222gw@student.lnu.se>
2019-11-28 13:29:18 +0100

    RED

--- reflexesTest/MainControllerTest.cs
+++ reflexesTest/MainControllerTest.cs
@@ -134,5 +134,20 @@ namespace reflexesTest
             mockConsoleView.Verify(view => view.DisplayLevelSelection(), Times.Once());
         }
 
+        [Fact]
+        public void RunApplication_ShouldCallEasyMode()
+        {
+            var mockReflexGame = new Mock<ReflexGame>();
+            var mockConsoleView = new Mock<ConsoleView>();
+            var mockLevelController = new Mock<LevelController>();
+
+            var mainController = new MainController(mockReflexGame.Object, mockConsoleView.Object, mockLevelController.Object);
+            mockConsoleView.SetupSequence(view => view.GetAction()).Returns(1).Returns(4);
+
+            mainController.RunApplication();
+            mockLevelController.Verify(levelcontroller => levelcontroller.EasyMode(), Times.Once());
+        }
+
+
     }
 }
